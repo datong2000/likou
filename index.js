7dd6537ebd36baf0dcc0b326908ac3e389278757
DT <2829099680@qq.com>
2023-02-20 09:04:27 +0800

    更新

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// --------------------------------------------------------------- 加一
+var plusOne = function (digits) {
+    let str = '';
+
+    for (let i = 0; i < digits.length; i++) {
+        str += digits[i];
+    }
+
+    str = parseInt(str) + 1;
+    digits = str.toString().split('');
+
+    for (let i = 0; i < digits.length; i++) {
+        digits[i] = parseInt(digits[i]);
+    }
+
+    return digits;
+};
+
+console.log(plusOne([1, 2, 9]));
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([4, 3, 2, 1]));
+console.log(plusOne([0]));
+console.log(plusOne([9]));
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+
+
 // --------------------------------------------------------------- 快乐数
 // var isHappy = function (n) {
 //     let nums = n.toString().split('');
